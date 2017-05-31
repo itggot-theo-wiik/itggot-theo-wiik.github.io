8e6809f6ff9f7c457d5fb647b6d6379a31db9e08
itggot-theo-wiik <theo.wiik@itggot.se>
2017-05-31 22:23:07 +0200

    font color

--- js/script.js
+++ js/script.js
@@ -2,19 +2,29 @@ function bg_color() {
     var random_number = Math.random();
 
     if (random_number < 0.33) {
-        bg_color_adder("cyan")
+        bg_color_adder("cyan", "black")
     } else if (0.33 < random_number && random_number < 0.66) {
-        bg_color_adder("pink")
+        bg_color_adder("pink", "black")
     } else {
         bg_color_adder("purple")
     }
 }
 
-function bg_color_adder(color) {
+function bg_color_adder(color, font_color) {
     element = document.querySelector("main");
     element.classList.add(color);
     element = document.querySelector("header");
     element.classList.add(color);
+    if (font_color == "black") {
+        element = document.querySelector("p");
+        element.classList.add("font_black");
+        element = document.querySelector("h1");
+        element.classList.add("font_black");
+        element = document.querySelector(".results");
+        element.classList.add("font_black");
+        element = document.querySelector(".version");
+        element.classList.add("font_black");
+    }
 }
 
 var drinks = ["Sprite", "Sprite Zero", "Powerking", "Zingo", "Zingo Passion", "Dr Pepper", "Mountain Dew Burk" , "Mountain Dew Flaska", "Trocadero Burk", "Trodacera Flaska", "Aloe Vera", "Vatten", "Eget val"];
